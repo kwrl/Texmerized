a9edde0d30d3710a224489982cff0998bd4b7f89
kwrl <haakongk@stud.ntnu.no>
2015-05-25 00:20:55 +0200

    Added obscure filter

--- public/javascripts/angularjs/controllers.js
+++ public/javascripts/angularjs/controllers.js
@@ -2,15 +2,13 @@
  * Created by haakonkaurel on 24/05/15.
  */
 
-var texmerizedApp = angular.module('texmerizedApp', []);
+var texmerizedApp = angular.module('texmerizedApp', ['texmerizedFilters']);
 
 texmerizedApp.controller('TextUnitController', function ($scope) {
-    $scope.textunits = [
-    ];
-
+    $scope.textunits = [];
+    $scope.stopwords = [];
 
     $scope.queryChanged = function () {
-        $scope.textunits = [];
         document.title = $scope.query;
         $.post("/query", {
             "queryID": $scope.queryID++,
@@ -19,19 +17,23 @@ texmerizedApp.controller('TextUnitController', function ($scope) {
                 data.text_units.forEach(function (elem) {
                     $scope.textunits.push({
                         title: elem.title,
-                        paragraphs : elem.paragraphs
+                        paragraphs: elem.paragraphs
                     });
                 });
             }
         );
     };
 
-}).directive('textunit', function() {
+}).directive('textunit', function () {
         return {
-            templateUrl : function(elem, attr) {
+            templateUrl: function (elem, attr) {
                 return 'directives/textunit.html';
             }
-        }
-    }
-
-);
+        };
+}).directive('obscure', function () {
+        return {
+            templateUrl: function(elem, attr) {
+               return 'directives/obscure-textunit.html';
+            }
+        };
+});
