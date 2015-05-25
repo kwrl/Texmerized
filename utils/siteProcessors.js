1c1455a67ab5e111c277a638beee0d24a191e444
kwrl <haakongk@stud.ntnu.no>
2015-05-25 14:50:32 +0200

    Reworked the default site processor and added some reuse of code

--- utils/siteProcessors.js
+++ utils/siteProcessors.js
@@ -2,45 +2,52 @@
  * Created by haakonkaurel on 24/05/15.
  */
 
-function default_site_processor($, url, cb) {
+function default_approach_by_root($, root, cb) {
     var processed_result = {
         links: [],
         text_units: []
     };
     if ($) {
-        $("p").each(function (i, elem) {
-            processed_result.text_units.push({title: $(this).text(), paragraphs: []});
-        });
-    }
-    cb(processed_result);
-}
-
-function wikipedia_site_processor($, url, cb) {
-    var processed_result = {
-        links: [],
-        text_units: []
-    };
-    if($) {
         var text_unit = {
             title: $("#firstHeading").text(),
             paragraphs: []
         };
 
-        $("#mw-content-text").children().each(function(i, elem) {
-            if($(elem).is("h2, h3")) {
-                if(text_unit.paragraphs.length > 0) {
+        $(root).children().each(function (i, elem) {
+            if ($(elem).is("h1, h2, h3, h4, h5, h6")) {
+                if (text_unit.paragraphs.length > 0) {
                     processed_result.text_units.push(text_unit);
                 }
                 text_unit = {
                     title: $(elem).text(),
                     paragraphs: []
                 };
-            } else if($(elem).is("p") && $(elem).text()!="") {
+            } else if ($(elem).is("p") && $(elem).text() != "") {
                 text_unit.paragraphs.push($(elem).text());
             }
         });
     }
     cb(processed_result);
+}
+
+function default_site_processor($, url, cb) {
+    var largest = -1;
+    var rootNode;
+    var childCount;
+    if ($) {
+        $("*").each(function (i, elem) {
+            childCount = $(elem).children("p, h2, h3").length;
+            if (childCount > largest) {
+                largest = childCount;
+                rootNode = elem;
+            }
+        });
+    }
+    default_approach_by_root($, rootNode, cb);
+}
+
+function wikipedia_site_processor($, url, cb) {
+    default_approach_by_root($, $("#mw-content-text"), cb);
 }
 
 var exports = module.exports;
